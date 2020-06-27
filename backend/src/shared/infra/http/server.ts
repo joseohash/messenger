47d6f694fb9de991a4fdac1a74b07724f1fa6c78
Jose <joseoctavioconti@gmail.com>
2020-06-27 20:45:08 -0300

    Tsyringe, Typeorm, AppError

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,13 +1,51 @@
-import express from 'express';
+import 'reflect-metadata';
+import 'dotenv/config';
+import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
+import 'express-async-errors';
+
 import Http from 'http';
 import io from 'socket.io';
+import AppError from '@shared/errors/AppError';
+
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 const server = new Http.Server(app);
 
-const socket = io(server);
+const ioSocket = io(server);
 
-socket.on('connection', () => console.log('alguem entrou'));
+ioSocket.on('connection', socket => {
+  socket.on('chat message', () => {
+    console.log(socket.id);
+  });
+});
 
+// eslint-disable-next-line no-console
 server.listen(3333, () => console.log('Server up on 3333'));
